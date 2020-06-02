195044d95eff3d96407ead390bf9691b2615dc7a
Robert <rlamacraft@gmail.com>
2020-06-02 12:52:51 +0100

    Code cleanup

---
+++ decoder.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+
+import os
+import itertools
+from parser import *
+
+class EncodingsSet():
+
+    def __init__(self, encodings, shared_bits):
+        self.encodings = encodings
+        self.shared_bits = shared_bits
+        # NOTE: the invariant that all encodings do share such bits is not asserted
+
+    def __len__(self):
+        return(len(self.encodings))
+
+    def __str__(self):
+        out = f"{len(self.encodings)} share the bits "
+        out += "{"
+        shared_bits_str = []
+        shared_bits_items = list(self.shared_bits.items())
+        shared_bits_items.sort()
+        for shared_bit, shared_value in shared_bits_items:
+            shared_value_str = "0" if shared_value == Bit.Zero else "1"
+            shared_bits_str.append(f"{shared_bit}:{shared_value_str}")
+        out += ", ".join(shared_bits_str)
+        out += "}\n"
+        for encoding in self.encodings:
+            out += f"- {str(encoding)} {encoding.instruction_set} ({encoding.instruction.name})[{encoding.instruction.fileName}]\n"
+        return(out)
+
+    def append(self, encoding):
+        self.encodings |= {encoding}
+
+    # returns two new encoding sets
+    def split(self, common_bit_position):
+        if common_bit_position in self.shared_bits.keys():
+            raise ValueError(common_bit_position, "is already shared by all members of this encoding set")
+
+        zeros_shared_bits = self.shared_bits.copy()
+        zeros_shared_bits[common_bit_position] = Bit.Zero
+        zeros = EncodingsSet(set(), zeros_shared_bits)
+
+        ones_shared_bits = self.shared_bits.copy()
+        ones_shared_bits[common_bit_position] = Bit.One
+        ones = EncodingsSet(set(), ones_shared_bits)
+
+        for encoding in self.encodings:
+            (bit_type, bit_value) = encoding.getBit(common_bit_position)
+            if   (bit_type == BitValueType.Unbound) \
+              or (bit_type == BitValueType.Bound and bit_value == Bit.Zero) \
+              or (bit_type == BitValueType.Unpredictably_bound and bit_value == Bit.Zero):
+                zeros.append(encoding)
+            if   (bit_type == BitValueType.Unbound) \
+              or (bit_type == BitValueType.Bound and bit_value == Bit.One) \
+              or (bit_type == BitValueType.Unpredictably_bound and bit_value == Bit.One):
+                ones.append(encoding)
+        return({zeros, ones})
+
+    # returns 2 ** n new encoding sets by splitting on n bit positions
+    def splitMany(self, common_bit_positions):
+        sets = {self}
+        for bit_position in common_bit_positions:
+            new_set = set()
+            for each_set in sets:
+                new_set |= each_set.split(bit_position)
+            sets = new_set
+        return(sets)
+
+    # in addition to the bits listed by self.shared_bits, there may be other bits that are commonly bound - split on those
+    def findOtherCommonlyBoundBits(self):
+        other_bits = set(range(32)) - set(self.shared_bits.keys())
+        unbound_in_an_instruction = set()
+        for position in other_bits:
+            for encoding in self.encodings:
+                (bitValueType, _) = encoding.getBit(position)
+                if bitValueType == BitValueType.Unbound:
+                    unbound_in_an_instruction |= {position}
+                    break
+        return(other_bits - unbound_in_an_instruction)
+
+    def splitOnCommonBoundBits(self):
+        return(self.splitMany(self.findOtherCommonlyBoundBits()))
+
+    def findDifferentBoundValue(self):
+       other_bits = set(range(32)) - set(self.shared_bits.keys())
+       different = set()
+       for position in other_bits:
+           values = set()
+           for encoding in self.encodings:
+               (_, bitValue) = encoding.getBit(position)
+               if bitValue != None:
+                   values |= {bitValue}
+           if len(values) == 2:
+                different |= {position}
+       return(different)
+
+    def splitOnDifferentBoundValue(self):
+       return(self.splitMany(self.findDifferentBoundValue()))
+
+def findCommonBitsAndSplitRecursively(encoding_set, depth):
+    if depth > 6:  # prevents infinite loops given aliases are indistinguishable
+        return {encoding_set}
+    encoding_subsets = encoding_set.splitOnCommonBoundBits()
+    encoding_subsubsets = set()
+    for subset in encoding_subsets:
+        if len(subset) > 1:      # i.e. more sub-dividing may be possible
+            encoding_subsubsets |= findCommonBitsAndSplitRecursively(subset, depth + 1)
+        else:
+            encoding_subsubsets |= {subset}
+    return(encoding_subsubsets)
+
+instructions = parseAllFiles()
+print("Parsed", len(instructions), "instructions.")
+
+# For purposes of intial prototyping, will we consider the encodings of only a subset of all of the instructions
+encodings = list(itertools.chain(*[inst.encodings for inst in instructions[0:-1]]))
+
+encoding_set = EncodingsSet(set(encodings), {})
+[print(str(encoding_set)) for encoding_set in list(findCommonBitsAndSplitRecursively(encoding_set, 0)) if len(encoding_set) > 0]
+# TODO: What about aliases?
---
+++ parser.py
@@ -0,0 +1,137 @@
+from enum import Enum
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import re
+
+class Bit(Enum):
+    Zero = 0
+    One = 1
+
+class BitSequenceType(Enum):
+    Sequence = 0
+    Inverted_Sequence = 1
+
+class BitValueType(Enum):
+    Unbound = 0               # denoted with "x", or an empty string
+    Bound = 1                 # denoted with either "0" or "1"
+    Unpredictably_bound = 2   # denoted with either "(0)" or "(1)"
+
+def readBitValue(string):
+    encodings = {
+        "x"  : (BitValueType.Unbound, None),
+        "0"  : (BitValueType.Bound, Bit.Zero),
+        "1"  : (BitValueType.Bound, Bit.One),
+        "(0)": (BitValueType.Unpredictably_bound, Bit.Zero),
+        "(1)": (BitValueType.Unpredictably_bound, Bit.One)
+    }
+    if string in encodings:
+        return(encodings[string])
+    raise ValueError("Unknown bit value: " + string)
+
+def strBitValue(bitValuePair):
+    decodings = {
+        (BitValueType.Unbound, None): "x",
+        (BitValueType.Bound, Bit.Zero): "0",
+        (BitValueType.Bound, Bit.One): "1",
+        (BitValueType.Unpredictably_bound, Bit.Zero): "(0)",
+        (BitValueType.Unpredictably_bound, Bit.One): "(1)"
+    }
+    if bitValuePair in decodings:
+        return(decodings[bitValuePair])
+    raise ValueError("Unknown bit value: " + bitValuePair)
+
+def getAttr(element, name, default):
+    """Similar API to getAttr, but for DOM Elements"""
+    return element.getAttribute(name) if element.hasAttribute(name) else default
+
+class XmlDecoder:
+
+    def parse(_xmlNode):
+        raise NotImplementedError("Object that can be instantiated from the XML should implement this")
+
+class BitSequence(XmlDecoder):
+
+    def __init__(self, xmlNode, isT16):
+        self.parse(xmlNode, isT16)
+
+    def __str__(self):
+        ret = ""
+        if self.inverted:
+            ret += "!"
+        ret += "".join([strBitValue(bitValue) for bitValue in self.constants])
+        return(ret)
+
+    def parse_values(self, constants):
+        if constants == []:
+            constants = ["x"] * self.width
+        if constants[0].startswith("!="):
+            self.inverted = True
+            constants = list(re.compile("!= (.*)").match(constants[0])[1])
+        else:
+            self.inverted = False
+        self.constants = [readBitValue(c) for c in constants]
+
+    def parse(self, xmlNode, isT16):
+        self.high_bit = int(xmlNode.getAttribute("hibit"))
+        if isT16:
+            self.high_bit = self.high_bit - 16
+        self.width = int(getAttr(xmlNode, "width", 1))
+        self.low_bit = self.high_bit - self.width + 1
+        self.name = getAttr(xmlNode, "name", "_")
+        self.parse_values([c.childNodes[0].data for c in xmlNode.getElementsByTagName("c") if len(c.childNodes) > 0 is not None])
+
+class Encoding(XmlDecoder):
+
+    def __init__(self, xmlNode, instruction):
+        self.instruction = instruction
+        self.parse(xmlNode)
+
+    def __str__(self):
+        return(":".join([str(seq) for seq in self.bitSequences]))
+
+    def parse(self, xmlNode):
+        regDiagram = xmlNode.getElementsByTagName("regdiagram")[0]
+        isT16 = regDiagram.getAttribute("form") == "16"
+        self.instruction_set = "T16" if isT16 else xmlNode.getAttribute("isa")
+        self.bitSequences = [BitSequence(boxNode, isT16) for boxNode in regDiagram.getElementsByTagName("box")]
+        self.total_bit_sequence_length = sum([seq.width for seq in self.bitSequences])
+
+    # NOTE: doesn't support inverted sequences
+    def getSequenceByBitIndex(self, index):
+        index_of_remainder = index
+        for bitSequence in self.bitSequences:
+            if index_of_remainder < bitSequence.width:
+                return bitSequence
+            index_of_remainder -= bitSequence.width
+        raise ValueError("index (" + index + ") > length of instruction (" + self.total_bit_sequence_length)
+
+    def getBit(self, index):
+        sequence = self.getSequenceByBitIndex(index)
+        return(sequence.constants[index - (31 - sequence.high_bit)])
+
+    def getBitRange(self, lower, upper):
+        return([self.getBit(i) for i in range(lower, upper)])
+
+class Instruction(XmlDecoder):
+
+    def __init__(self, fileName, xmlNode):
+        self.fileName = fileName
+        self.parse(xmlNode)
+
+    def parse(self, instNode):
+        self.name = instNode.getElementsByTagName("heading")[0].childNodes[0].data
+        self.encodings = [Encoding(iclass, self) for iclass in instNode.getElementsByTagName("iclass")]
+
+def parseInstruction(xmlDir, xmlFile):
+    path = xmlDir + xmlFile
+    xml_data = xml.dom.minidom.parse(path)
+    return(Instruction(xmlFile, xml_data))
+
+def parseAllFiles():
+	xmlDir = "spec/ISA_v82A_A64_xml_00bet3.1/"
+	indexFile = xmlDir + "index.xml"
+	indexXml = xml.dom.minidom.parse(indexFile)
+	iforms = indexXml.getElementsByTagName('iform')
+	xmlFiles = [iform.getAttribute('iformfile') for iform in iforms]
+	instructions = [parseInstruction(xmlDir, xmlFile) for xmlFile in xmlFiles]
+	return(instructions)
