537e96b6bd3ed1a15e81b54206eab975d5e159e0
Robert <rlamacraft@gmail.com>
2020-06-04 13:46:22 +0100

    TestDecoder: test_append and test_split

--- test_decoder.py
+++ test_decoder.py
@@ -1,5 +1,25 @@
 import unittest
 from decoder import *
+from test_parser import *
+
+class TestEncodingsSet(unittest.TestCase):
+
+    def test_append(self):
+        encoding_set = EncodingsSet(set(), {})
+        encoding = Encoding(parseSingleNode(TestEncoding.xml()).childNodes[0], None)
+        encoding_set.append(encoding)
+        self.assertEqual(encoding_set.encodings, set([encoding]))
+
+    def test_split(self):
+        encoding_zero = Encoding(parseSingleNode(TestEncoding.xml()).childNodes[0], None)
+        encoding_one = Encoding(parseSingleNode(TestEncoding.xml().replace('0','1',1)).childNodes[0], None)
+        encoding_set = EncodingsSet(set([encoding_zero, encoding_one]), {})
+        sets = encoding_set.split(0)
+        for enc_set in sets:
+           shared_bits = enc_set.shared_bits
+           self.assertEqual(len(enc_set.encodings), 1)
+           encoding = list(enc_set.encodings)[0]
+           self.assertEqual(encoding.getBit(0), (BitValueType.Bound, shared_bits[0]))
 
 def main():
     unittest.main()
