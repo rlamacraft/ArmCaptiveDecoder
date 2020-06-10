33644d2a7637b10d1a583027c484c170bf5f73d6
Robert <rlamacraft@gmail.com>
2020-06-10 17:36:57 +0100

    Plan c++ code with print out of encodings set

--- code_generator.py
+++ code_generator.py
@@ -1,9 +1,14 @@
 from jinja2 import Environment, FileSystemLoader
 
+def prepend_lines(multiline_string, prefix):
+    """prepend a string before every line i.e. after every newline char"""
+    return(prefix + multiline_string.replace('\n', '\n' + prefix))
+
 def generate_code(encodings_sets):
     env = Environment(
         loader=FileSystemLoader('.'),
         line_statement_prefix='#'
     )
+    env.filters['prepend_lines'] = prepend_lines
     template = env.get_template('decoder.cpp.jinja')
     return(template.render(sets=encodings_sets))
