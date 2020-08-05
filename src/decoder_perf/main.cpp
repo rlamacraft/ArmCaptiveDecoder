8d2034b6359a95976ebd2420becc144bba3f6c48
Robert <rlamacraft@gmail.com>
2020-08-05 17:59:38 +0100

    Stream program for performance testing current decoder

--- src/decoder_perf/main.cpp
+++ src/decoder_perf/main.cpp
@@ -7,10 +7,14 @@
 
 using namespace captive::arch::arm64;
 
-#define ONE_MB_INST_DATA 25 * 100 * 1000
-#define NUM_OF_INSTRUCTIONS ONE_MB_INST_DATA
+// #define ONE_MB_INST_DATA 25 * 100 * 1000
+// #define NUM_OF_INSTRUCTIONS ONE_MB_INST_DATA
 
-int disasm(uint32_t ir) {
+#define LINUX_TEXT 3485966
+#define HELLO_WORLD_TEXT 109
+#define NUM_OF_INSTRUCTIONS HELLO_WORLD_TEXT
+
+int just_decode(uint32_t ir) {
   captive::arch::arm64::arm64_decode_a64 decoder;
   decoder.isa_mode = arm64_decode_a64::arm64_a64;
   uint64_t pc = 0x0000000000000000;
@@ -27,22 +31,22 @@ int main(int argc, char *argv[]) {
   }
 
   unsigned char bytes[4];
-  int sum = 0;
   FILE *fp = fopen(argv[1], "rb");
-  uint32_t inst_data[NUM_OF_INSTRUCTIONS];
   int index = 0;
+
+  clock_t begin = clock();
+
   while( fread(bytes, 4, 1, fp) != 0) {
     if(index == NUM_OF_INSTRUCTIONS) {
       printf("Reading more instructions than expected\n");
       return 1;
     }
-    inst_data[index++] = bytes[0] | (bytes[1]<<8) | (bytes[2]<<16) | (bytes[3]<<24);
+    just_decode(bytes[0] | (bytes[1]<<8) | (bytes[2]<<16) | (bytes[3]<<24));
+    index++;
   }
-
-  clock_t begin = clock();
-
-  for(int i = 0; i < NUM_OF_INSTRUCTIONS; i++) {
-    disasm(inst_data[i]);
+  if(index != NUM_OF_INSTRUCTIONS) {
+    printf("Didn't read enough data\n");
+    return 1;
   }
 
   clock_t end = clock();
